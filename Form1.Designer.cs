dbed0e9464cdeb187bbc19c9b938c33db4791f47
kekwem <93526125+kekwem@users.noreply.github.com>
2021-12-24 20:17:33 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace choinka
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel = new System.Windows.Forms.Panel();
+            this.buttonDrzewo = new System.Windows.Forms.Button();
+            this.buttonPrezent = new System.Windows.Forms.Button();
+            this.buttonGwiazda = new System.Windows.Forms.Button();
+            this.buttonBombki = new System.Windows.Forms.Button();
+            this.buttonLancuchy = new System.Windows.Forms.Button();
+            this.buttonSwiatelka = new System.Windows.Forms.Button();
+            this.buttonUsun = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Location = new System.Drawing.Point(12, 12);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(536, 426);
+            this.panel.TabIndex = 0;
+            this.panel.Paint += new System.Windows.Forms.PaintEventHandler(this.panel_Paint);
+            // 
+            // buttonDrzewo
+            // 
+            this.buttonDrzewo.Location = new System.Drawing.Point(599, 30);
+            this.buttonDrzewo.Name = "buttonDrzewo";
+            this.buttonDrzewo.Size = new System.Drawing.Size(75, 23);
+            this.buttonDrzewo.TabIndex = 1;
+            this.buttonDrzewo.Text = "Drzewo";
+            this.buttonDrzewo.UseVisualStyleBackColor = true;
+            this.buttonDrzewo.Click += new System.EventHandler(this.buttonDrzewo_Click);
+            // 
+            // buttonPrezent
+            // 
+            this.buttonPrezent.Location = new System.Drawing.Point(599, 112);
+            this.buttonPrezent.Name = "buttonPrezent";
+            this.buttonPrezent.Size = new System.Drawing.Size(75, 23);
+            this.buttonPrezent.TabIndex = 2;
+            this.buttonPrezent.Text = "Prezent";
+            this.buttonPrezent.UseVisualStyleBackColor = true;
+            this.buttonPrezent.Click += new System.EventHandler(this.buttonPrezent_Click);
+            // 
+            // buttonGwiazda
+            // 
+            this.buttonGwiazda.Location = new System.Drawing.Point(599, 168);
+            this.buttonGwiazda.Name = "buttonGwiazda";
+            this.buttonGwiazda.Size = new System.Drawing.Size(75, 23);
+            this.buttonGwiazda.TabIndex = 3;
+            this.buttonGwiazda.Text = "Gwiazda";
+            this.buttonGwiazda.UseVisualStyleBackColor = true;
+            this.buttonGwiazda.Click += new System.EventHandler(this.buttonGwiazda_Click);
+            // 
+            // buttonBombki
+            // 
+            this.buttonBombki.Location = new System.Drawing.Point(599, 221);
+            this.buttonBombki.Name = "buttonBombki";
+            this.buttonBombki.Size = new System.Drawing.Size(75, 23);
+            this.buttonBombki.TabIndex = 4;
+            this.buttonBombki.Text = "Bombki";
+            this.buttonBombki.UseVisualStyleBackColor = true;
+            this.buttonBombki.Click += new System.EventHandler(this.buttonBombki_Click_1);
+            // 
+            // buttonLancuchy
+            // 
+            this.buttonLancuchy.Location = new System.Drawing.Point(599, 70);
+            this.buttonLancuchy.Name = "buttonLancuchy";
+            this.buttonLancuchy.Size = new System.Drawing.Size(75, 23);
+            this.buttonLancuchy.TabIndex = 5;
+            this.buttonLancuchy.Text = "Lancuchy";
+            this.buttonLancuchy.UseVisualStyleBackColor = true;
+            this.buttonLancuchy.Click += new System.EventHandler(this.buttonLancuchy_Click);
+            // 
+            // buttonSwiatelka
+            // 
+            this.buttonSwiatelka.Location = new System.Drawing.Point(599, 273);
+            this.buttonSwiatelka.Name = "buttonSwiatelka";
+            this.buttonSwiatelka.Size = new System.Drawing.Size(75, 23);
+            this.buttonSwiatelka.TabIndex = 6;
+            this.buttonSwiatelka.Text = "Swiatelka";
+            this.buttonSwiatelka.UseVisualStyleBackColor = true;
+            this.buttonSwiatelka.Click += new System.EventHandler(this.buttonSwiatelka_Click);
+            // 
+            // buttonUsun
+            // 
+            this.buttonUsun.Location = new System.Drawing.Point(599, 378);
+            this.buttonUsun.Name = "buttonUsun";
+            this.buttonUsun.Size = new System.Drawing.Size(75, 23);
+            this.buttonUsun.TabIndex = 7;
+            this.buttonUsun.Text = "Usun";
+            this.buttonUsun.UseVisualStyleBackColor = true;
+            this.buttonUsun.Click += new System.EventHandler(this.buttonUsun_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(583, 322);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(75, 23);
+            this.axWindowsMediaPlayer1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(722, 452);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.buttonUsun);
+            this.Controls.Add(this.buttonSwiatelka);
+            this.Controls.Add(this.buttonLancuchy);
+            this.Controls.Add(this.buttonBombki);
+            this.Controls.Add(this.buttonGwiazda);
+            this.Controls.Add(this.buttonPrezent);
+            this.Controls.Add(this.buttonDrzewo);
+            this.Controls.Add(this.panel);
+            this.Name = "Form1";
+            this.Text = "Choinka";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button buttonDrzewo;
+        private System.Windows.Forms.Button buttonPrezent;
+        private System.Windows.Forms.Button buttonGwiazda;
+        private System.Windows.Forms.Button buttonBombki;
+        private System.Windows.Forms.Button buttonLancuchy;
+        private System.Windows.Forms.Button buttonSwiatelka;
+        private System.Windows.Forms.Button buttonUsun;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+namespace choinka
+{
+    public partial class Form1 : Form
+    {
+        WindowsMediaPlayer player = new WindowsMediaPlayer();
+
+        bool drzewo = false;
+        bool prezent = false;
+        bool gwiazda = false;
+        bool bombki = false;
+        bool lancuchy = false;
+        bool swiatelka = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            player.URL = @"C:\Users\Dimon\source\repos\choinka\We-wish-you-a-merry-Christmas.mp3";
+        }
+
+        private void buttonDrzewo_Click(object sender, EventArgs e)
+        {
+            drzewo = true;
+            Graphics g = panel.CreateGraphics();
+            Point point1 = new Point(200, 30);
+            Point point2 = new Point(130, 100);
+            Point point3 = new Point(270, 100);
+            Point point4 = new Point(200, 70);
+            Point point5 = new Point(130, 140);
+            Point point6 = new Point(270, 140);
+            Point point7 = new Point(200, 110);
+            Point point8 = new Point(130, 180);
+            Point point9 = new Point(270, 180);
+            Point[] tablica1 =
+            {
+                point1,
+                point2,
+                point3,
+            };
+            Point[] tablica2 =
+            {
+                point4,
+                point5,
+                point6
+            };
+            Point[] tablica3 =
+            {
+                point7,
+                point8,
+                point9
+            };
+            g.FillPolygon(Brushes.Green, tablica1);
+            g.FillPolygon(Brushes.Green, tablica2);
+            g.FillPolygon(Brushes.Green, tablica3);
+            g.FillRectangle(Brushes.Brown, 180, 180, 40, 40);
+        }
+
+        private void buttonPrezent_Click(object sender, EventArgs e)
+        {
+            prezent = true;
+            Graphics g = panel.CreateGraphics();
+            g.FillRectangle(Brushes.DarkCyan, 120, 200, 50, 30);
+            g.FillRectangle(Brushes.Blue, 120, 210, 50, 10);
+            g.FillRectangle(Brushes.Blue, 140, 200, 10, 30);
+
+            g.FillRectangle(Brushes.Red, 160, 190, 60, 50);
+            g.FillRectangle(Brushes.Yellow, 160, 210, 60, 10);
+            g.FillRectangle(Brushes.Yellow, 180, 190, 20, 50);
+
+            g.FillRectangle(Brushes.Pink, 230, 200, 30, 30);
+            g.FillRectangle(Brushes.Red, 230, 210, 30, 10);
+            g.FillRectangle(Brushes.Red, 240, 200, 10, 30);
+        }
+
+        private void buttonGwiazda_Click(object sender, EventArgs e)
+        {
+            gwiazda = true;
+            Graphics g = panel.CreateGraphics();
+            Point point1 = new Point(200, 15);
+            Point point2 = new Point(210, 35);
+            Point point3 = new Point(230, 35);
+            Point point4 = new Point(210, 40);
+            Point point5 = new Point(220, 60);
+            Point point6 = new Point(200, 45);
+            Point point7 = new Point(180, 60);
+            Point point8 = new Point(190, 40);
+            Point point9 = new Point(170, 35);
+            Point point10 = new Point(190, 35);
+            Point[] tablica = {
+                point1,
+                point2,
+                point3,
+                point4,
+                point5,
+                point6,
+                point7,
+                point8,
+                point9,
+                point10
+            };
+            g.FillPolygon(Brushes.Yellow, tablica);
+        }
+
+        Point[] tablica_bombek = null;
+        int[] tablica_kolorow = null;
+
+        private void buttonBombki_Click_1(object sender, EventArgs e)
+        {
+            bombki = true;
+            Random r = new Random();
+            Graphics g = panel.CreateGraphics();
+            if (tablica_bombek == null)
+            {
+                tablica_bombek = new Point[7];
+                tablica_kolorow = new int[7];
+                for (int i = 0; i < 7; i++)
+                {
+                    int x = r.Next(170, 230);
+                    int y = r.Next(70, 175);
+                    tablica_bombek[i] = new Point(x, y);
+                    int color = r.Next(1, 4);
+                    if (color == 1)
+                    {
+                        g.FillEllipse(Brushes.Pink, x, y, 10, 10);
+                    }
+                    else if (color == 2)
+                    {
+                        g.FillEllipse(Brushes.Blue, x, y, 10, 10);
+                    }
+                    else
+                    {
+                        g.FillEllipse(Brushes.Orange, x, y, 10, 10);
+                    }
+                    tablica_kolorow[i] = color;
+                    g.DrawLine(Pens.Black, x + 5, y, x + 5, y - 5);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < tablica_bombek.Length; i++)
+                {
+                    if (tablica_kolorow[i] == 1)
+                    {
+                        g.FillEllipse(Brushes.Pink, tablica_bombek[i].X, tablica_bombek[i].Y, 10, 10);
+                    }
+                    else if (tablica_kolorow[i] == 2)
+                    {
+                        g.FillEllipse(Brushes.Blue, tablica_bombek[i].X, tablica_bombek[i].Y, 10, 10);
+                    }
+                    else
+                    {
+                        g.FillEllipse(Brushes.Orange, tablica_bombek[i].X, tablica_bombek[i].Y, 10, 10);
+                    }
+                    g.DrawLine(Pens.Black, tablica_bombek[i].X + 5, tablica_bombek[i].Y, tablica_bombek[i].X + 5, tablica_bombek[i].Y - 5);
+                }
+            }
+            
+        }
+
+        private void panel_Paint(object sender, PaintEventArgs e)
+        {
+            if (drzewo) buttonDrzewo_Click(sender, e);
+            if (lancuchy) buttonLancuchy_Click(sender, e);
+            if (prezent) buttonPrezent_Click(sender, e);
+            if (gwiazda) buttonGwiazda_Click(sender, e);
+            if (bombki) buttonBombki_Click_1(sender, e);
+            if (swiatelka) buttonSwiatelka_Click(sender, e);
+        }
+
+        private void buttonLancuchy_Click(object sender, EventArgs e)
+        {
+            lancuchy = true;
+            Graphics g = panel.CreateGraphics();
+            Point[] tablica = {
+                new Point(220, 55),
+                new Point(170, 100),
+            };
+            g.DrawCurve(new Pen(Brushes.White, 5f), tablica);
+            Point[] tablica1 = {
+                new Point(240, 110),
+                new Point(150, 160),
+            };
+            g.DrawCurve(new Pen(Brushes.White, 5f), tablica1);
+        }
+
+        private void buttonSwiatelka_Click(object sender, EventArgs e)
+        {
+            swiatelka = true;
+            Graphics g = panel.CreateGraphics();
+            Point[] tablica =
+            {
+                new Point(170, 60),
+                new Point(180, 70),
+                new Point(190, 80),
+                new Point(200, 90),
+                new Point(210, 100),
+                new Point(220, 110),
+                new Point(230, 120),
+                new Point(170, 130),
+                new Point(180, 140),
+                new Point(190, 150),
+                new Point(200, 160),
+                new Point(210, 170)
+            };
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    g.FillEllipse(Brushes.Red, tablica[i].X, tablica[i].Y, 7, 7);
+                }
+                else if (i % 3 == 1)
+                {
+                    g.FillEllipse(Brushes.Yellow, tablica[i].X, tablica[i].Y, 7, 7);
+                }
+                else
+                {
+                    g.FillEllipse(Brushes.GreenYellow, tablica[i].X, tablica[i].Y, 7, 7);
+                }
+                
+            }
+        }
+
+        private void buttonUsun_Click(object sender, EventArgs e)
+        {
+            tablica_bombek = null;
+            tablica_kolorow = null;
+            drzewo = false;
+            prezent = false;
+            gwiazda = false;
+            bombki = false;
+            lancuchy = false;
+            panel.Refresh();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            player.controls.play();
+        }
+    }
+}
